df7c5ad15a6ccb958d9c0ec1ef341a16444772d5
Rabies Cow <rabiescow@proton.me>
2025-04-15 05:24:39 -0400

    updated looks for git and vi, also added support for long directories

--- functions/__vertical_component_dir.fish
+++ functions/__vertical_component_dir.fish
@@ -2,12 +2,28 @@ function __vertical_component_dir
   __vertical_util_set vertical_dir_color        blue --bold
   __vertical_util_set vertical_dir_prefix       ' '
   __vertical_util_set vertical_dir_prefix_color blue --bold
-  __vertical_util_set vertical_dir_trunc_git    false
+  __vertical_util_set vertical_dir_trunc_git    true
   __vertical_util_set vertical_dir_trunc_home   true
   __vertical_util_set vertical_dir_suffix       ""
   __vertical_util_set vertical_dir_suffix_color blue
 
-  set -l dir (prompt_pwd $PWD)
+  set -l dir
+  if [ $vertical_dir_trunc_git = true ] && __vertical_util_is_git
+    # Find the current directory with all symlinks resolved
+    set dir (pwd -P 2> /dev/null || pwd)
+    # Find the path to the root of the git repository. The idea behind the
+    # string replace is to replace the first part of the path (up to the
+    # git root directory) with just the basename of the root directory.
+    set -l root (command git rev-parse --show-toplevel 2> /dev/null)
+    set dir (string replace $root (basename $root) $dir)
+  else if [ $vertical_dir_trunc_home = true ]
+    # Replace $HOME with ~
+    set -l realhome ~
+    set dir (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+    set dir (prompt_pwd --full-length-dirs 2 $dir)
+  else
+    set dir (prompt_pwd --full-length-dirs 2 $PWD)
+  end
 
   set_color normal
   set_color $vertical_dir_prefix_color
